3514b45444e373ab5b8ea3a3aa55e32969cb3039
Simon Baslé <sbasle@pivotal.io>
2018-04-15 19:13:47 +0200

    Add permissive security config with two fake users

---
+++ src/main/java/com/example/place/server/auth/SecurityConfig.java
@@ -0,0 +1,52 @@
+package com.example.place.server.auth;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.ReactiveAuthenticationManager;
+import org.springframework.security.authentication.UserDetailsRepositoryReactiveAuthenticationManager;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+/**
+ * @author Simon Baslé
+ */
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+	@Bean
+	public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+		return http.csrf()
+		           .disable()
+		           .formLogin()
+		           .and()
+		           .authorizeExchange()
+		           .anyExchange()
+		           .permitAll()
+		           .and()
+		           .build();
+	}
+
+	@Bean
+	public ReactiveAuthenticationManager manager(
+			@Autowired ReactiveUserDetailsService userDetailsService) {
+		return new UserDetailsRepositoryReactiveAuthenticationManager(userDetailsService);
+	}
+
+	@Bean
+	public ReactiveUserDetailsService userDetailsService() {
+		return new MapReactiveUserDetailsService(User.withDefaultPasswordEncoder()
+		                                             .username("admin")
+		                                             .password("admin")
+		                                             .roles("USER", "ADMIN")
+		                                             .build(),
+				User.withDefaultPasswordEncoder()
+				    .username("foo")
+				    .password("bar")
+				    .roles("USER")
+				    .build());
+	}
+}
